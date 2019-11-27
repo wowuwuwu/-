5139ad1bb135a6032d38f86f5552a7c3915645d8
wowuwuwu <40024940+wowuwuwu@users.noreply.github.com>
2019-11-27 19:56:42 +0800

    楼层

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,14 +3,15 @@ Page({
         // 存放轮播图数据
         swiperList:[],
         // 导航列表
-        navsList:[]
+        navsList:[],
+        // 楼层数据
+        floorList:[]
     },
     onLoad(){  
         // 发送异步请求 请求轮播图数据
         wx.request({
             url:"https://api.zbztb.cn/api/public/v1/home/swiperdata",
             success:(result)=>{
-                // console.log(result);
                 this.setData({
                     swiperList: result.data.message
                 })
@@ -19,9 +20,17 @@ Page({
         wx.request({
             url:"https://api.zbztb.cn/api/public/v1/home/catitems",
             success:(result)=>{
-                console.log(result);
                 this.setData({
                     navsList:result.data.message
+                })
+            }
+        }),
+        wx.request({
+            url:"https://api.zbztb.cn/api/public/v1/home/floordata",
+            success:(result)=>{
+                console.log(result);
+                this.setData({
+                    floorList:result.data.message
                 })
             }
         })
