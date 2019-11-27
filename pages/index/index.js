3830b2d631fc4918014f8bc4963ba1802618c986
wowuwuwu <40024940+wowuwuwu@users.noreply.github.com>
2019-11-27 19:05:59 +0800

    导航部分

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,9 @@
 Page({
     data: {
         // 存放轮播图数据
-        swiperList:[]
+        swiperList:[],
+        // 导航列表
+        navsList:[]
     },
     onLoad(){  
         // 发送异步请求 请求轮播图数据
@@ -11,6 +13,15 @@ Page({
                 // console.log(result);
                 this.setData({
                     swiperList: result.data.message
+                })
+            }
+        }),
+        wx.request({
+            url:"https://api.zbztb.cn/api/public/v1/home/catitems",
+            success:(result)=>{
+                console.log(result);
+                this.setData({
+                    navsList:result.data.message
                 })
             }
         })
