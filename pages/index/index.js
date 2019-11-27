ffb772be635eb1c73064e5e9eafae081db987468
wowuwuwu <40024940+wowuwuwu@users.noreply.github.com>
2019-11-27 18:40:53 +0800

    首页轮播图及公共样式

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,1 +1,19 @@
-Page({})
+Page({
+    data: {
+        // 存放轮播图数据
+        swiperList:[]
+    },
+    onLoad(){  
+        // 发送异步请求 请求轮播图数据
+        wx.request({
+            url:"https://api.zbztb.cn/api/public/v1/home/swiperdata",
+            success:(result)=>{
+                // console.log(result);
+                this.setData({
+                    swiperList: result.data.message
+                })
+            }
+        })
+    }
+});
+  
